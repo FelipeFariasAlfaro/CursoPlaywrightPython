440227f2eb0141b8464819419d69884d506a4bfb
FelipeFariasAlfaro <farias3felipe@gmail.com>
2026-08-03 18:59:50 -0400

    Sesion 11, primeros clicks en la web de pruebas

--- features/steps/compra.py
+++ features/steps/compra.py
@@ -1,9 +1,17 @@
 from behave import given, when, then
 from helpers.locator_loader import load_locators
 
-#locators = load_locators('tiendaqa')
+selectores = load_locators('tiendaqa.json')
 
 @given('ingreso a la página "{url}"')
 def step_ingreso_a_pagina(context, url):
     context.page.goto(url)
+
+@given(u'hago click en "{element}"') #primer_producto
+def step_impl(context, element):
+   elemento_web = context.page.locator(selectores[element])
+   elemento_web.click()
+   context.page.wait_for_timeout(5000)
+
+
 
--- features/tiendaQA.feature
+++ features/tiendaQA.feature
@@ -3,3 +3,5 @@ Feature: Flujo de compra E2E
   @centyc-e2e
   Scenario: Flujo de compra happy path
     Given ingreso a la página "https://centyc.cl/tiendaqa"
+    And hago click en "ver_todos_los_productos" 
+    And hago click en "primer_producto" 
